273236236c44c7f0d1d401fbe9b82a20a49c56af
Josiah Burchard <josiah@jburchard.com>
2017-02-08 15:38:48 -0600

    refactor(partials)

--- gulpFile.js
+++ gulpFile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 var changed = require('gulp-changed');
-var imagemin =require('gulp-imagemin');
+var imagemin = require('gulp-imagemin');
 var inlineSource = require('gulp-inline-source');
 var filter = require('gulp-filter'); // Will phase out filter in favor of gIf
 var uncss = require('gulp-uncss');
@@ -17,7 +17,7 @@ var fs = require('fs');
 
 var DIST = 'dist/';
 
-gulp.task('build', function(callback) {
+gulp.task('build', function (callback) {
     run(
         'sitemap',
         'build-assets',
@@ -27,13 +27,16 @@ gulp.task('build', function(callback) {
     );
 });
 
-gulp.task('build-views', function() {
-    var html = filter(['**/*.html'], {restore: true});
+gulp.task('build-views', function () {
+    var html = filter(['**/*.html'], { restore: true });
 
     return gulp.src(['src/**/*.html', '!src/partials/**'])
         .pipe(inject(gulp.src(['src/partials/head.html']), createInjectOptions('head')))
-        .pipe(useref({searchPath: ['.', '../', 'src']}))
-        .pipe(inlineSource({rootpath: 'dist'}))
+        .pipe(inject(gulp.src(['src/partials/footer.html']), createInjectOptions('footer')))
+        .pipe(inject(gulp.src(['src/partials/scripts.html']), createInjectOptions('scripts')))
+        .pipe(inject(gulp.src(['src/partials/cta.html']), createInjectOptions('cta')))
+        .pipe(useref({ searchPath: ['.', '../', 'src'] }))
+        .pipe(inlineSource({ rootpath: 'dist' }))
         .pipe(html)
         .pipe(htmlmin({
             collapseWhitespace: true,
@@ -43,17 +46,17 @@ gulp.task('build-views', function() {
         .pipe(gulp.dest(DIST));
 });
 
-gulp.task('build-assets', function() {
-    var js = filter(['**/*.js'], {restore: true});
-    var css = filter(['**/*.css'], {restore: true});
-    var images = filter(['**/*.gif', '**/*.jpg', '**/*.svg', '**/*.png'], {restore: true});
-    
+gulp.task('build-assets', function () {
+    var js = filter(['**/*.js'], { restore: true });
+    var css = filter(['**/*.css'], { restore: true });
+    var images = filter(['**/*.gif', '**/*.jpg', '**/*.svg', '**/*.png'], { restore: true });
+
     return gulp.src('src/**/*.*')
         .pipe(js)
         .pipe(uglify())
         .pipe(js.restore)
         .pipe(css)
-        .pipe(uncss({html: ['src/**/*.html'], ignore: ['#sidebar.menu-active>.inner.menu', '#sidebar.menu-active']}))
+        .pipe(uncss({ html: ['src/**/*.html'], ignore: ['#sidebar.menu-active>.inner.menu', '#sidebar.menu-active'] }))
         .pipe(csso())
         .pipe(css.restore)
         .pipe(images)
@@ -62,22 +65,22 @@ gulp.task('build-assets', function() {
         .pipe(gulp.dest(DIST))
 });
 
-gulp.task('sitemap', function(done) {
+gulp.task('sitemap', function (done) {
     var sm = require('sitemap');
 
     var pages = [
-        { url: '/'},
+        { url: '/' },
     ];
- 
+
     var sitemap = sm.createSitemap({
-        hostname: 'https://todo.todo', 
+        hostname: 'https://todo.todo',
         urls: pages
     });
 
-    if (!fs.existsSync(DIST)){
+    if (!fs.existsSync(DIST)) {
         fs.mkdirSync(DIST);
     }
- 
+
     fs.writeFileSync(DIST + 'sitemap.xml', sitemap.toString());
     return done();
 });
@@ -91,46 +94,46 @@ function createInjectOptions(tagName) {
     };
 }
 
-gulp.task('generate-service-worker-dist', function(callback) {
-  writeServiceWorkerFile(DIST, true, callback);
+gulp.task('generate-service-worker-dist', function (callback) {
+    writeServiceWorkerFile(DIST, true, callback);
 });
 
 function writeServiceWorkerFile(rootDir, handleFetch, callback) {
-  var config = {
-    cacheId: 'website-com-cache',
-    handleFetch: handleFetch,
-    runtimeCaching: [{
-      // See https://github.com/GoogleChrome/sw-toolbox#methods
-      urlPattern: /(images|lib)\//,
-      handler: 'cacheFirst',
-      // See https://github.com/GoogleChrome/sw-toolbox#options
-      options: {
-        cache: {
-          name: 'runtime-cache'
+    var config = {
+        cacheId: 'website-com-cache',
+        handleFetch: handleFetch,
+        runtimeCaching: [{
+            // See https://github.com/GoogleChrome/sw-toolbox#methods
+            urlPattern: /(images|lib)\//,
+            handler: 'cacheFirst',
+            // See https://github.com/GoogleChrome/sw-toolbox#options
+            options: {
+                cache: {
+                    name: 'runtime-cache'
+                }
+            }
+        }],
+        staticFileGlobs: [
+            rootDir + '/index.html',
+            rootDir + '/**.css',
+            rootDir + '/**.js',
+        ],
+        stripPrefix: rootDir,
+        // verbose defaults to false, but for the purposes of this demo, log more.
+        verbose: true
+    };
+
+    swPrecache.generate(config, function (err, fileString) {
+        if (err) {
+            return console.log('Uh Oh! Trouble writing the service worker pre cache!');
         }
-      }
-    }],
-    staticFileGlobs: [
-      rootDir + '/index.html',
-      rootDir + '/**.css',
-      rootDir + '/**.js',
-    ],
-    stripPrefix: rootDir,
-    // verbose defaults to false, but for the purposes of this demo, log more.
-    verbose: true
-  };
 
-  swPrecache.generate(config, function(err, fileString) {
-      if (err) {
-          return console.log('Uh Oh! Trouble writing the service worker pre cache!');
-      }
+        let formattedFileString = fileString.replace(/(index)?\.html"/gi, '"');
 
-      let formattedFileString = fileString.replace(/(index)?\.html"/gi, '"');
-
-        fs.writeFileSync(path.join(rootDir, 'service-worker.js'), formattedFileString, function(err) {
-            if(err) {
+        fs.writeFileSync(path.join(rootDir, 'service-worker.js'), formattedFileString, function (err) {
+            if (err) {
                 return console.log(err);
             }
-        }); 
-  });
+        });
+    });
 }
