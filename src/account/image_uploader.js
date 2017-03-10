d1d021dbe66db1d8501885d17cd641a33af82a11
tfickas <tfickas@gmail.com>
2017-03-10 15:13:45 -0600

    Update image_uploader.js

--- src/account/image_uploader.js
+++ src/account/image_uploader.js
@@ -1,9 +1,10 @@
-var storageRef = firebase.storage().ref();
+function uploadFile () {
+  var storageRef = firebase.storage().ref();
 
 
-var file = File();
-ref.put(file).then(function(snapshot) {
-  console.log('New image has been uploaded!');
-});
+  var file = File();
+  ref.put(file).then(function(snapshot) {
+    console.log('New image has been uploaded!');
+})};
 
 
