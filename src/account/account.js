950478d56d7ec1dc92ed74582a0b448a1581b44a
Josiah Burchard <josiah@jburchard.com>
2017-04-09 20:47:52 -0500

    feat(image-uploading): add uploading

--- src/account/account.js
+++ src/account/account.js
@@ -6,47 +6,35 @@ function redirectToLogin() {
   window.location.href = '/account/login';
 }
 
-// var storageRef = firebase.storage().ref('image_gallery/');
+$('.file-uploader').fileupload({
+  dataType: 'json',
+  add: function (e, data) {
+    uploadToFirebase($(e.target).data('gallery'), data.files)
+    // We are specifically not doing this
+    // data.submit();
+  },
+});
 
-// var file = File();
-// storageRef.put(file).then(function(snapshot) {
-//   console.log('New image has been uploaded!');
+function uploadToFirebase(gallery, files) {
+  files.forEach(function (file) {
+    var filePath = 'image_gallery/' + file.name;
+    var storageRef = firebase.storage().ref(filePath);
 
+    var uploadTask = storageRef.put(file);
 
-//   var uploadTask;
-//  // Register three observers:
-// // 1. 'state_changed' observer, called any time the state changes
-// // 2. Error observer, called on failure
-// // 3. Completion observer, called on successful completion
-// uploadTask.on('state_changed', function(snapshot){
-//   // Observe state change events such as progress, pause, and resume
-//   // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-//   var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//   console.log('Upload is ' + progress + '% done');
-//   switch (snapshot.state) {
-//     case firebase.storage.TaskState.PAUSED: // or 'paused'
-//       console.log('Upload is paused');
-//       break;
-//     case firebase.storage.TaskState.RUNNING: // or 'running'
-//       console.log('Upload is running');
-//       break;
-//   }
-// }, function(error) {
-//   // Handle unsuccessful uploads
-// }, function() {
-//   // Handle successful uploads on complete
-//   // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-//   var postKey = firebase.database().ref('Posts/').push().key;
-//   var downloadURL = uploadTask.snapshot.downloadURL;
-//   var updates = {};
-//   var postData = {
-//   	url: downloadURL,
-//   };
+    uploadTask.on('state_changed', function (snapshot) {
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+    }, function (error) {
+      alert('Uh oh! We had a problem uploading this file')
+    }, function () {
+      addToIndex(gallery, filePath)
+    });
+  })
+}
 
-//   updates ['/Posts/'+postKey] = postData;
-//   firebase.database().ref.update(updates);
-//   console.log(downloadURL);
-// });
-// });
-
-
+function addToIndex(index, filePath) {
+  debugger;
+  var dbRef = firebase.database().ref(index);
+  dbRef.push(filePath);
+}
